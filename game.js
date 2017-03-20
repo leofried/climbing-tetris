0349b6d7bd43ec8b4fc87982c3112c13326b40e2
lsfnewyork <lsffuture@gmail.com>
2017-03-19 22:45:44 -0400

    Built Canvas and Start Button

---
+++ game.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+	var button = document.getElementById("startButton");
+	button.setAttribute("onClick", "startGame();");
+}
+
+function startGame(){
+	console.log("as");
+	alert("here");
+}
