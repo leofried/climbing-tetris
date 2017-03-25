bbe81c9d718b505d68d94c20ce9d969a4830100f
lsfnewyork <lsffuture@gmail.com>
2017-03-24 22:53:05 -0400

    Finish Previous Commit
    Not quite sure what happened, the previous commit did not commit fully.

--- main.js
+++ main.js
@@ -14,13 +14,41 @@ window.onload = function() {
 var canvas;
 var context;
 
+/*Variables to do with the size of the screen. The width and height of the canvas in blocks,
+the width and height of the canvas in pixels, and the width and height of a block in pixels.*/
+var blockWidth = 10;
+var blockHeight = 20;
+
+var pixelWidth = 300;
+var pixelHeight = 500;
+
+var pbWidth = pixelWidth / blockWidth;
+var pbHeight = pixelHeight / blockHeight;
+
+
+//An array that contains the canvas as represented in blocks, (0, 0) is the bottom left corner (1, 0) is still in the floor.
+//0 is empty, 1 is player, 2 is ground, 3 is piece.
+var array = new Array(blockWidth);
+
+for(var i=0; i<blockWidth; i++){
+	array[i] = new Array(blockHeight);
+	for(var j=0; j<blockHeight; j++){
+		array[i][j] = 0;
+	}
+}
+array[0][blockHeight - 1] = 1;
 
 
 
 
+//Starts the game, sets the intervals, adds the listeners, etc.
+function startGame() {
+	setInterval(updateFrame, 1);
+	document.addEventListener("click", mouseClick, false);
+}
 
 
-
+//What happens when the mouse gets clicked. The mouse getting clicked is the main source of user input.
 function mouseClick(event) {
 	var rect = canvas.getBoundingClientRect();
 	var clickX = event.clientX - rect.left;
@@ -32,6 +60,7 @@ function mouseClick(event) {
 	}
 }
 
+//Update the canvas frame.
 function updateFrame() {
 
 	for(var i=0; i<array.length; i++){
